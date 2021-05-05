d8a16a56c482b2b764c55967f8952bfba288623f
Alejandro <willywes@hotmail.cl>
2021-05-05 15:20:50 -0400

    [ADD] AboutUS

--- resources/react/webapp/components/general/H2Title.js
+++ resources/react/webapp/components/general/H2Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const H2Title = ({text, className}) =>{
     return (
-        <h2 className={`font-poppins font-21 bold text-center uppercase text-primary ${className}`}>{text}</h2>
+        <h2 className={`font-poppins font-21 bold text-center uppercase text-primary ${className}`} style={{letterSpacing : '2px'}}>{text}</h2>
     );
 };
 
--- resources/react/webapp/pages/public/AboutUs/index.js
+++ resources/react/webapp/pages/public/AboutUs/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import PUBLIC_ROUTES from "../../../routes/publicRoutes";
 import BasePanelOne from "../../../template/BasePanelOne";
 import Subscribe from "../../../components/sections/Subscribe";
 import {CONFIG} from "../../../Config";
 import eurekaLogo from '../../../assets/images/pages/about-us/eureka-logo.png'
+import H2Title from "../../../components/general/H2Title";
+import handsWhite from '../../../assets/images/icons/hands-white.svg'
+import {Tabs, Tab} from 'react-bootstrap';
+
 
 const AboutUs = () => {
+
+    const [key, setKey] = useState('vision');
 
     let breadcrumbs = [
         {
@@ -24,40 +30,120 @@ const AboutUs = () => {
             <BasePanelOne
                 breadcrumbs={breadcrumbs}
             >
-                <div className="row">
-                    <div className="col-3">
+                <div className="px-3">
+                    <div className="row pb-5 mb-5">
+                        <div className="col-3">
 
-                    </div>
-                    <div className="col-9">
-                        <h3 className="font-poppins font-22 bold color-033F5D mb-3">
-                            Wireframes can be pencil drawings or sketches on a whiteboard
-                        </h3>
-                        <div className="font-poppins font-18 regular color-6C6B6B lh-30">
-                            Wireframes can be pencil drawings or sketches on a whiteboard, or they can be produced by
-                            means of a broad array of free or commercial software applications. Wireframes are generally
-                            created by business analysts, user experience designers, developers, visual designers, and
-                            by those with expertise in interaction design, information architecture and user research.
+                        </div>
+                        <div className="col-9">
+                            <h3 className="font-poppins font-22 bold color-033F5D mb-3">
+                                Wireframes can be pencil drawings or sketches on a whiteboard
+                            </h3>
+                            <div className="font-poppins font-18 regular color-6C6B6B lh-30">
+                                Wireframes can be pencil drawings or sketches on a whiteboard, or they can be produced
+                                by
+                                means of a broad array of free or commercial software applications. Wireframes are
+                                generally
+                                created by business analysts, user experience designers, developers, visual designers,
+                                and
+                                by those with expertise in interaction design, information architecture and user
+                                research.
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="row">
-                    <div className="col-3">
-
-                    </div>
-                    <div className="col-9">
-
-                        <div className="mb-3">
-                            <img src={eurekaLogo}  alt={CONFIG.APP_NAME}/>
+                    <div className="row pb-5 mb-5">
+                        <div className="col-12 pb-5">
+                            <H2Title text="VALORES" className="font-30"/>
                         </div>
-                        <h3 className="font-poppins font-22 bold color-033F5D mb-3">
-                            Una alianza
-                        </h3>
-                        <div className="font-poppins font-18 regular color-6C6B6B lh-30">
-                            Wireframes can be pencil drawings or sketches on a whiteboard, or they can be produced by
-                            means of a broad array of free or commercial software applications. Wireframes are generally
-                            created by business analysts, user experience designers, developers, visual designers, and
-                            by those with expertise in interaction design, information architecture and user research.
+                        {
+                            [1, 2, 3].map((_, i) => {
+                                return <div key={i} className="col-md-4 d-flex">
+                                    <div className="row">
+                                        <div className="col-12 mb-5">
+                                            <div className="circle-about-us mx-auto">
+                                                <img className="m-auto" src={handsWhite} alt={CONFIG.APP_NAME}/>
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <div className="font-poppins font-14 regular">
+                                                Wireframes can be pencil drawings or sketches on a whiteboard, or they
+                                                can be produced by means of a broad array of free or commercial software
+                                                applications. Wireframes
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </div>
+
+                    <div className="row pb-5 mb-5">
+                        <div className="col">
+                            <Tabs
+                                id="vision-mission"
+                                variant="pills"
+                                activeKey={key}
+                                onSelect={(k) => setKey(k)}
+                            >
+                                <Tab eventKey="vision" title="Visión">
+                                    <div className="panel-bordered p-5">
+                                        <p className="font-poppins font-14 regular">
+                                            Entregar un servicio de salud y bienestar, en un ambiente de cordialidad y
+                                            respeto por las personas, con el propósito de cimentar relaciones de largo
+                                            plazo con nuestros colaboradores y clientes.
+                                        </p>
+                                        <p className="font-poppins font-14 regular">
+                                            Wireframes can be pencil drawings or sketches on a whiteboard, or they can
+                                            be produced by means of a broad array of free or commercial software
+                                            applications. Wireframes are generally created by business analysts, user
+                                            experience designers, developers, visual designers, and by those with
+                                            expertise in interaction design, information architecture and user research.
+                                        </p>
+                                    </div>
+                                </Tab>
+                                <Tab eventKey="mission" title="Misión">
+                                    <div className="panel-bordered p-5">
+                                        <p className="font-poppins font-14 regular">
+                                            Wireframes can be pencil drawings or sketches on a whiteboard, or they can
+                                            be produced by means of a broad array of free or commercial software
+                                            applications. Wireframes are generally created by business analysts, user
+                                            experience designers, developers, visual designers, and by those with
+                                            expertise in interaction design, information architecture and user research.
+                                        </p>
+                                        <p className="font-poppins font-14 regular">
+                                            Entregar un servicio de salud y bienestar, en un ambiente de cordialidad y
+                                            respeto por las personas, con el propósito de cimentar relaciones de largo
+                                            plazo con nuestros colaboradores y clientes.
+                                        </p>
+                                    </div>
+                                </Tab>
+                            </Tabs>
+                        </div>
+                    </div>
+
+
+                    <div className="row pb-5 mb-5">
+                        <div className="col-3">
+
+                        </div>
+                        <div className="col-9">
+                            <div className="mb-5">
+                                <img src={eurekaLogo} alt={CONFIG.APP_NAME}/>
+                            </div>
+                            <h3 className="font-poppins font-22 bold color-033F5D mb-3">
+                                Una alianza
+                            </h3>
+                            <div className="font-poppins font-18 regular color-6C6B6B lh-30">
+                                Wireframes can be pencil drawings or sketches on a whiteboard, or they can be produced
+                                by
+                                means of a broad array of free or commercial software applications. Wireframes are
+                                generally
+                                created by business analysts, user experience designers, developers, visual designers,
+                                and
+                                by those with expertise in interaction design, information architecture and user
+                                research.
+                            </div>
                         </div>
                     </div>
                 </div>
