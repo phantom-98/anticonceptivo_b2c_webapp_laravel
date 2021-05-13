42b32371dc57dec21f2f730c71419bb7c04e7851
Felipe <fe.ferngarc@gmail.com>
2021-05-13 11:45:04 -0400

    [ADD] AC-15 need to fix the css & dropdown

--- resources/react/webapp/template/layouts/Header/HeaderNavbar.js
+++ resources/react/webapp/template/layouts/Header/HeaderNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 // import pastillas from '../../../assets/images/icons/navbar/pastillas.svg'
 // import masculino from '../../../assets/images/icons/navbar/masculino.svg'
 // import testEmbarazo from '../../../assets/images/icons/navbar/test-embarazo.svg'
@@ -7,12 +7,22 @@ import React, { useState, useEffect } from 'react';
 // import anticonceptivo from '../../../assets/images/icons/navbar/anticonceptivo.svg'
 // import femenino from '../../../assets/images/icons/navbar/femenino.svg'
 import HeaderNavbarItem from "../../components/HeaderNavbarItem";
+import {Dropdown} from 'react-bootstrap'
 import PUBLIC_ROUTES from "../../../routes/publicRoutes";
 import * as Services from "../../../Services";
+import { v4 as uuidv4 } from 'uuid';
 
 const HeaderNavbar = () => {
 
     const [categories, setCategories] = useState([]);
+    const [show, setShow] = useState(false);
+
+    const showDropdown = (e)=>{
+        setShow(!show);
+    }
+    const hideDropdown = e => {
+        setShow(false);
+    }
 
     useEffect(() => {
         getCategories();
@@ -32,6 +42,12 @@ const HeaderNavbar = () => {
         });
     }
 
+    const CustomToggle = React.forwardRef(({children, onClick}, ref) => (
+        <div className="pointer" ref={ref} onClick={(e)=> { e.preventDefault(); onClick(e); }}>
+            {children}
+        </div>
+    ));
+
     return (
         <div className="header-navbar bg-0869A6">
             <div className="container">
@@ -41,7 +57,32 @@ const HeaderNavbar = () => {
                             let url = PUBLIC_ROUTES.SHOP.path;
                             url = url.replace(":category", category.slug);
                             return(
-                                <HeaderNavbarItem linkTo={url} icon={category.image} text={category.name} key={category.id}/>
+                                <Dropdown key={uuidv4()}
+                                    show={show}
+                                    onMouseEnter={showDropdown} 
+                                    onMouseLeave={hideDropdown}
+                                >
+                                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                                        <HeaderNavbarItem linkTo={url} icon={category.image} text={category.name}/>
+                                    </Dropdown.Toggle>
+                                    {
+                                        category.subcategories.length ? 
+                                            <Dropdown.Menu align="right" bsPrefix="dropdown-menu-custom">
+                                                {
+                                                    category.subcategories.map((subCategory) => {
+                                                        let childUrl = PUBLIC_ROUTES.SHOP.path;
+                                                        url = url.replace(":category", subCategory.slug);
+                                                        return(
+                                                            <Dropdown.Item key={uuidv4()}>
+                                                                <HeaderNavbarItem linkTo={childUrl} icon={subCategory.image} text={subCategory.name}/>
+                                                            </Dropdown.Item>
+                                                        )
+                                                    })
+                                                }
+                                            </Dropdown.Menu>
+                                        : null
+                                    }
+                                </Dropdown>                                    
                             )
                         })
                     }
