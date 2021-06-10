de62b92048b97b5c7d69989860487a8d1c42227a
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-06-10 16:11:55 -0400

    [FIX] Rutas

--- routes/intranet.php
+++ routes/intranet.php
@@ -79,9 +79,9 @@ if (env('SHOW_INTRANET', 'TRUE') == 'TRUE') {
                 Route::post('categorias/active', 'CategoryController@active')->name('categories.active');
                 Route::resource('categorias', 'CategoryController', ['names' => getResourceRoutesForNameHelper('categories')]);
 
-                Route::post('subcategorias/position', 'SubCategoryController@position')->name('subcategories.position');
-                Route::post('subcategorias/active', 'SubCategoryController@active')->name('subcategories.active');
-                Route::resource('subcategorias', 'SubCategoryController', ['names' => getResourceRoutesForNameHelper('subcategories')]);
+                Route::post('subcategorias/position', 'SubcategoryController@position')->name('subcategories.position');
+                Route::post('subcategorias/active', 'SubcategoryController@active')->name('subcategories.active');
+                Route::resource('subcategorias', 'SubcategoryController', ['names' => getResourceRoutesForNameHelper('subcategories')]);
 
                 Route::post('marcas/active', 'BrandController@active')->name('brands.active');
                 Route::post('marcas/position', 'BrandController@position')->name('brands.position');
