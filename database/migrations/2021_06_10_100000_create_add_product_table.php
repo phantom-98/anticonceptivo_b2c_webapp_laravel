99a226ceff5ddcdcfbc004789355f4f9b72ad5cb
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-06-11 12:27:56 -0400

    Update 2021_06_10_100000_create_add_product_table.php

--- database/migrations/2021_06_10_100000_create_add_product_table.php
+++ database/migrations/2021_06_10_100000_create_add_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddToProductsTableTwo extends Migration
+class CreateAddProductTable extends Migration
 {
     /**
      * Run the migrations.
