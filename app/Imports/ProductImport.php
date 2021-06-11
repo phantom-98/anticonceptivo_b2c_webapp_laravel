a48fad9fcbb5fe626af2fed30889dfe5cada570d
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-06-11 18:31:53 -0400

    Fix carga masiva

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -17,57 +17,59 @@ class ProductImport implements ToCollection, WithHeadingRow
     {
         foreach ($rows as $row) 
         {            
-            $product = Product::where('sku', $row['sku'])->first();
-            
-            if($product){
-                $laboratory = Laboratory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['laboratorio'])."%'")->first(); 
-                $subcategory = Subcategory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['subcategoria'])."%'")->first(); 
+            if($row['sku']){
+                $product = Product::where('sku', $row['sku'])->first();
+                
+                if($product){
+                    $laboratory = Laboratory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['laboratorio'])."%'")->first(); 
+                    $subcategory = Subcategory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['subcategoria'])."%'")->first(); 
 
-                $product->name = $row['nombre'];
-                $product->slug = \Str::slug($row['nombre']);
-                $product->price = $row['precio'];
-                $product->sku = $row['sku'];
-                $product->offer_price = $row['precio_oferta'];
-                $product->is_offer = $row['precio_oferta'] > 0 ? 1 : 0;
-                $product->subcategory_id = $subcategory->id;
-                $product->consumption_typology = $row['tipologia_consumo'] ?? 'ABA - ORAL S.ORD.GRAGEAS';
-                $product->compound = $row['composicion'];
-                $product->benefits = $row['beneficios'];
-                $product->data_sheet = $row['ficha_tecnica'];
-                $product->description = $row['descripcion'];
-                $product->is_bioequivalent = $row['bioequivalente'] == "Si" ? 1 : 0;
-                $product->laboratory_id = $laboratory->id;
-                $product->format = $row['formato'];
-                $product->barcode = $row['codigo_de_barras'];
-                $product->unit_format = $row['formato_unidad'];
-                $product->recipe_type = $row['tipo_de_receta'];
-                $product->state_of_matter = $row['estado'];
-                $product->save();
-            } else {
-                $product = new Product();
-                $laboratory = Laboratory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['laboratorio'])."%'")->first(); 
-                $subcategory = Subcategory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['subcategoria'])."%'")->first(); 
+                    $product->name = $row['nombre'];
+                    $product->slug = \Str::slug($row['nombre']);
+                    $product->price = $row['precio'];
+                    $product->sku = $row['sku'];
+                    $product->offer_price = $row['precio_oferta'];
+                    $product->is_offer = $row['precio_oferta'] > 0 ? 1 : 0;
+                    $product->subcategory_id = $subcategory->id;
+                    $product->consumption_typology = $row['tipologia_consumo'] ?? 'ABA - ORAL S.ORD.GRAGEAS';
+                    $product->compound = $row['composicion'];
+                    $product->benefits = $row['beneficios'];
+                    $product->data_sheet = $row['ficha_tecnica'];
+                    $product->description = $row['descripcion'];
+                    $product->is_bioequivalent = $row['bioequivalente'] == "Si" ? 1 : 0;
+                    $product->laboratory_id = $laboratory->id;
+                    $product->format = $row['formato'];
+                    $product->barcode = $row['codigo_de_barras'];
+                    $product->unit_format = $row['formato_unidad'];
+                    $product->recipe_type = $row['tipo_de_receta'];
+                    $product->state_of_matter = $row['estado'];
+                    $product->save();
+                } else {
+                    $product = new Product();
+                    $laboratory = Laboratory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['laboratorio'])."%'")->first(); 
+                    $subcategory = Subcategory::whereRaw("UPPER(name) LIKE '%". strtoupper($row['subcategoria'])."%'")->first(); 
 
-                $product->name = $row['nombre'];
-                $product->slug = \Str::slug($row['nombre']);
-                $product->price = $row['precio'];
-                $product->sku = $row['sku'];
-                $product->offer_price = $row['precio_oferta'];
-                $product->is_offer = $row['precio_oferta'] > 0 ? 1 : 0;
-                $product->subcategory_id = $subcategory->id;
-                $product->consumption_typology = $row['tipologia_consumo'] ?? 'ABA - ORAL S.ORD.GRAGEAS';
-                $product->compound = $row['composicion'];
-                $product->benefits = $row['beneficios'];
-                $product->data_sheet = $row['ficha_tecnica'];
-                $product->description = $row['descripcion'];
-                $product->is_bioequivalent = $row['bioequivalente'] == "Si" ? 1 : 0;
-                $product->laboratory_id = $laboratory->id;
-                $product->format = $row['formato'];
-                $product->barcode = $row['codigo_de_barras'];
-                $product->unit_format = $row['formato_unidad'];
-                $product->recipe_type = $row['tipo_de_receta'];
-                $product->state_of_matter = $row['estado'];
-                $product->save();
+                    $product->name = $row['nombre'];
+                    $product->slug = \Str::slug($row['nombre']);
+                    $product->price = $row['precio'];
+                    $product->sku = $row['sku'];
+                    $product->offer_price = $row['precio_oferta'];
+                    $product->is_offer = $row['precio_oferta'] > 0 ? 1 : 0;
+                    $product->subcategory_id = $subcategory->id;
+                    $product->consumption_typology = $row['tipologia_consumo'] ?? 'ABA - ORAL S.ORD.GRAGEAS';
+                    $product->compound = $row['composicion'];
+                    $product->benefits = $row['beneficios'];
+                    $product->data_sheet = $row['ficha_tecnica'];
+                    $product->description = $row['descripcion'];
+                    $product->is_bioequivalent = $row['bioequivalente'] == "Si" ? 1 : 0;
+                    $product->laboratory_id = $laboratory->id;
+                    $product->format = $row['formato'];
+                    $product->barcode = $row['codigo_de_barras'];
+                    $product->unit_format = $row['formato_unidad'];
+                    $product->recipe_type = $row['tipo_de_receta'];
+                    $product->state_of_matter = $row['estado'];
+                    $product->save();
+                }
             }
         }
     }
