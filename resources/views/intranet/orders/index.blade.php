8bccff8cfaeda71db1f1f4a1ce62e21a1bc2e89b
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-07-14 14:37:30 -0400

    Update index.blade.php

--- resources/views/intranet/orders/index.blade.php
+++ resources/views/intranet/orders/index.blade.php
@@ -208,11 +208,13 @@
                                                 </a>
                                             @endpush
                                             @else
+                                                @if($object->status != "CREATED")
                                                 <a onclick="change_status({{$object->id}}, '{{$object->status}}')"
                                                     class="btn btn-sm btn-default btn-hover-info add-tooltip"
                                                     title="Rechazar Recetas">
                                                         <i class="fa fa-retweet"></i>
                                                 </a>
+                                                @endif
                                             @endif
                                             @endcan
                                             @include('intranet.template.components._crud_html_actions_buttons')
