cbe8438898a6af8b3db8953cfdedc7ae181ea74e
xvad <xvad_@live.com>
2021-08-04 22:04:56 -0400

    ADDED: logica recursivo

--- resources/react/webapp/pages/public/ContactUs/Nested.js
+++ resources/react/webapp/pages/public/ContactUs/Nested.js
@@ -4,37 +4,23 @@ const Nested = ({children, path, setPath, list}) => {
 
     const handleChildren = (e) => {
         const found = list.find(x => x.id == e.target.value)
-
-        // grandparent = 4 = 0
-        // child 1 = 1
-        // child.parent_id == e.target.value ?
-        // evaluar si child.parent_id esta en el path
-        // index - key : slice
-        // const newPath
-        // setPath
-
-        console.log('value',e.target.value);
-        console.log('path',path);
-
         var temp_path = [];
-
-        path.forEach((element, index) => {
-            let position = element.children.findIndex(pa => pa.id == e.target.value);
-            if (position != -1) {
-                return index;
-            }
+        let position = -1;
+        let isNew = true
+        path.every((element, index) => {
+            position = element.children.findIndex(pa => pa.id == found.id);
             temp_path.push(element);
+            if(index + 1 != path.length && position != -1 ){
+                temp_path.push(found);
+                isNew = false
+                return isNew
+            }
+            return isNew
         });
-
-        console.log('temp path:',temp_path);
-
-        // path.forEach((element, index) => {
-        //     if (index <= externalPosition) {
-        //         temp_path.push(element);
-        //     }
-        // });
-
-        setPath(path => [...path, found]);   
+        if(isNew){
+            temp_path.push(found);
+        }
+        setPath(temp_path)
     }
 
     return(
