4d8f8fb7b5a68fc07f4c94978d4de2cd8e87ccd7
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-09-30 15:52:20 -0300

    Update CartReducer.js

--- resources/react/webapp/context/CartProvider/CartReducer.js
+++ resources/react/webapp/context/CartProvider/CartReducer.js
@@ -32,7 +32,7 @@ export default (state, action) => {
                 "positionClass": "toast-top-right",
                 "preventDuplicates": true,
                 "onclick": null,
-                "showDuration": "300",
+                "showDuration": "2000",
                 "hideDuration": "1000",
                 "timeOut": "1000",
                 "extendedTimeOut": "1000",
