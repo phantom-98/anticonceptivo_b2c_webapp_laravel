d8a4bacb9ff052f5380e11c0d26496bfdbedb411
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-10-06 11:11:26 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,21 @@ Route::get('test-ailoo', [TestController::class, 'AiloTest']);
 Route::get('PaySubscription-test', [TestController::class, 'PaySubscription']);
 Route::view('/{path?}/{pathTwo?}/{pathThree?}/{pathFour?}/{pathFive?}/{pathSix?}/{pathSeven?}', 'webapp.base_react');
 
+Route::get('fix-orders-payment/{id}', function ($id) {
+    $order = Order::find($id);
+    if($order->status == "CREATED"){
+        $order->status = PaymentStatus::PAID;
+        $order->payment_date = Carbon::now();
+        $order->payment_type = 'webpay';
+        $order->is_paid = true;
+        $order->save();
+    }
+
+    $customerAddress = CustomerAddress::with('commune')->where('customer_id',$order->customer_id)->where('default_address',1)->get()->first();
+    CallIntegrationsPay::callVoucher($order->id,$customerAddress);
+    CallIntegrationsPay::callUpdateStockProducts($order->id);
+    CallIntegrationsPay::callDispatchLlego($order->id,$customerAddress);
+    CallIntegrationsPay::sendEmailsOrder($order->id);
+
+    return "Done";
+});
