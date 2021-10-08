21bd3d272da672bbd06212addf8fac89fdaae03f
Felipe <fe.ferngarc@gmail.com>
2021-10-08 20:35:13 -0300

    [FIX] name

--- database/migrations/2021_10_07_112811_add_field_to_customers_table.php
+++ database/migrations/2021_10_07_112811_add_field_to_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldToCustomers extends Migration
+class AddFieldToCustomersTable extends Migration
 {
     /**
      * Run the migrations.
