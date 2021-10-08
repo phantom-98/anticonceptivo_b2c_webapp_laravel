1ca58a09039e881fced3abaa9e8ff384b0f5d9ae
Willywes <willywes@hotmail.cl>
2021-10-08 20:05:14 -0300

    [FIXED] Modo amazon

--- resources/react/webapp/components/shopping/ProductCard.js
+++ resources/react/webapp/components/shopping/ProductCard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import {CONFIG} from "../../Config";
 import {formatMoney} from "../../helpers/GlobalUtils";
 import PUBLIC_ROUTES from "../../routes/publicRoutes";
@@ -11,37 +11,80 @@ const ProductCard = ({product, className = ''}) => {
     const [quantity, setQuantity] = useState(1);
 
     return (
-        <div className={`product-card ${className}`}>
-            <div className="product-card-image">
-                <Link to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
-                      style={{textDecoration: 'none', color: '#000000'}}>
-                    <img src={product.images.length ? product.images[0].public_file : noImage} alt={`${CONFIG.APP_NAME} - ${product.name}`}/>
-                </Link>
-            </div>
-            <div className="product-card-body">
-                <div className="product-card-brand">{product.laboratory.name}</div>
-                <div className="product-card-name">
-                    <Link  to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
+        <Fragment>
+            <div className={`d-none d-md-block product-card ${className}`}>
+                <div className="product-card-image">
+                    <Link to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
                           style={{textDecoration: 'none', color: '#000000'}}>
-                        <div className="col-md-12 text-truncate p-0">
-                            {product.name}
-
-                        </div>
-                        {/*{String(product.name).length >= 23 ? product.name.substring(0, 23) + '...' : product.name }*/}
+                        <img src={product.images.length ? product.images[0].public_file : noImage} alt={`${CONFIG.APP_NAME} - ${product.name}`}/>
                     </Link>
                 </div>
-                <div className="product-card-price">{formatMoney(product.is_offer ? product.offer_price : product.price)}
-                    {
-                        product.is_offer ?
-                            <span className="font-poppins font-16 bold color-009BE8"><s>{' '}{formatMoney(product.price)}</s></span>
-                        : null
-                    }
+                <div className="product-card-body">
+                    <div className="product-card-brand">{product.laboratory.name}</div>
+                    <div className="product-card-name">
+                        <Link  to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
+                               style={{textDecoration: 'none', color: '#000000'}}>
+                            <div className="col-md-12 text-truncate p-0">
+                                {product.name}
+
+                            </div>
+                            {/*{String(product.name).length >= 23 ? product.name.substring(0, 23) + '...' : product.name }*/}
+                        </Link>
+                    </div>
+                    <div className="product-card-price">{formatMoney(product.is_offer ? product.offer_price : product.price)}
+                        {
+                            product.is_offer ?
+                                <span className="font-poppins font-16 bold color-009BE8"><s>{' '}{formatMoney(product.price)}</s></span>
+                                : null
+                        }
+                    </div>
+                </div>
+                <div className="product-card-cart">
+                    <AddCartCard quantity={quantity} setQuantity={setQuantity} product={product}/>
                 </div>
             </div>
-            <div className="product-card-cart">
-                <AddCartCard quantity={quantity} setQuantity={setQuantity} product={product}/>
+
+            {/* mobile*/}
+
+            <div className={`d-md-none d-block product-card ${className}`}>
+                <div className="row">
+                    <div className="col-auto">
+                        <Link to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
+                              style={{textDecoration: 'none', color: '#000000'}}>
+                            <img className="mobile-producto-img" src={!product.images.length ? product.images[0].public_file : noImage} alt={`${CONFIG.APP_NAME} - ${product.name}`}/>
+                        </Link>
+                    </div>
+                    <div className="col">
+                        <div className="product-card-brand">{product.laboratory.name}</div>
+                        <div className="product-card-name">
+                            <Link  to={(PUBLIC_ROUTES.PRODUCT_DETAIL.path).replace(':slug?', product.slug)}
+                                   style={{textDecoration: 'none', color: '#000000'}}>
+                                <div className="col-md-12 text-truncate p-0">
+                                    {product.name}
+
+                                </div>
+                                {/*{String(product.name).length >= 23 ? product.name.substring(0, 23) + '...' : product.name }*/}
+                            </Link>
+                        </div>
+                        <div className="product-card-price">{formatMoney(product.is_offer ? product.offer_price : product.price)}
+                            {
+                                product.is_offer ?
+                                    <span className="font-poppins font-16 bold color-009BE8"><s>{' '}{formatMoney(product.price)}</s></span>
+                                    : null
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="row pt-3">
+                    <div className="col">
+                        <div className="product-card-cart">
+                            <AddCartCard quantity={quantity} setQuantity={setQuantity} product={product}/>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>
+        </Fragment>
     );
 };
 
