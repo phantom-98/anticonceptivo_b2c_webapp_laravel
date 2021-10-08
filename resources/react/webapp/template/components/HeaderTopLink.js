2a9a6628cf7970842acc4aa9d463a0c68bf95ccf
Willywes <willywes@hotmail.cl>
2021-10-08 20:58:40 -0300

    [FIXED] Menu on Safari

--- resources/react/webapp/template/components/HeaderTopLink.js
+++ resources/react/webapp/template/components/HeaderTopLink.js
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 
 const HeaderTopLink = ({icon, text, linkTo = '#', flag, email}) =>{
     return (
-        <div className="col-md-auto top-do-flex">
+        <div className="col-md-auto top-do-flex" style={{ height : 33}}>
             {
-                flag ? 
+                flag ?
                     email ?
                     <div className="nav-item">
                         <a className="my-auto top-link pl-1 pr-0" style={{lineHeight: '33px'}} href="mailto:contacto@anticonceptivo.cl"><Icon path={icon}/>
@@ -19,8 +19,8 @@ const HeaderTopLink = ({icon, text, linkTo = '#', flag, email}) =>{
                             <span className="top-link pl-1">{' '}(2) 3245 1883</span>
                         </a>
                     </div>
-                    
-                : 
+
+                :
                     <div className="my-auto">
                         <Link to={linkTo}>{icon ? <Icon path={icon}/> : null} <span className="top-link">{text}</span></Link>
                     </div>
