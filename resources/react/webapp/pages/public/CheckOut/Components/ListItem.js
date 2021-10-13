da37e05633a0d5a42ba72744d576dd5aa4e438f3
Felipe <fe.ferngarc@gmail.com>
2021-10-13 10:49:44 -0300

    [ADD] dot separator

--- resources/react/webapp/pages/public/CheckOut/Components/ListItem.js
+++ resources/react/webapp/pages/public/CheckOut/Components/ListItem.js
@@ -32,7 +32,7 @@ const ListItem = ({address, showEdit, saveDefaultAddress, regions, communes}) =>
                     </div>
                     <div className="col-12">
                         <span className="font-poppins font-16 regular color-484848">
-                            {address.address}, {address.extra_info ? address.extra_info+ ' , ' : ''} {commune ? commune.name : null} , {region ? region.name : null}
+                            {address.address} , {address.extra_info ? address.extra_info+ ' , ' : ''} {commune ? commune.name : null} , {region ? region.name : null}
                         </span>
                     </div>
                 </div>
