ddf03ac9de86f0c61f7cb3abbbd5bba1b678f0ac
Felipe <fe.ferngarc@gmail.com>
2021-10-13 09:14:20 -0300

    [ADD] comment

--- app/Http/Controllers/Api/V1/App/Customer/ProfileController.php
+++ app/Http/Controllers/Api/V1/App/Customer/ProfileController.php
@@ -59,6 +59,8 @@ class ProfileController extends Controller
                 return ApiResponse::NotFound(null, OutputMessage::CUSTOMER_NOT_FOUND);
             }
 
+            // CAMBIAR A WHERE IN PARA AGREGAR MÁS REGIONES
+
             $regions = Region::where('id',7)->with('provinces.communes')->get();
 
             return ApiResponse::JsonSuccess([
