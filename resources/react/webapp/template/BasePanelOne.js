dc96d4328f40027d1d1e334cb9f9aa10c65c416c
Felipe <fe.ferngarc@gmail.com>
2021-10-14 15:54:01 -0300

    [ADD] logout responsive

--- resources/react/webapp/template/BasePanelOne.js
+++ resources/react/webapp/template/BasePanelOne.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Breadcrumbs from "../components/Breadcrumbs";
+import UseWindowDimensions from "../components/customHooks/UseWindowDimensions";
+import {Link} from "react-router-dom";
+import {AuthContext} from "../context/AuthProvider";
+
 
 const BasePanelOne = (props) => {
+
+    const { width } = UseWindowDimensions();
+    const { logout } = useContext(AuthContext)
 
     return (
         <div style={{background : '#FAFAFA'}}>
@@ -12,11 +19,32 @@ const BasePanelOne = (props) => {
                 <div className="panel mb-4">
                     <div className="responsive-base-panel" style={props.style}>
                         <div className="row">
-                            <div className="col-md-12">
-                                {
-                                    props.title ? <h1 className="base-panel-one-title">{props.title}</h1> : null
-                                }
-                            </div>
+                            {
+                                width < 768 ?
+                                <>
+                                    <div className="col-8">
+                                        {
+                                            props.title ? 
+                                                <h1 className="base-panel-one-title">{props.title}</h1> 
+                                            : null
+                                        }
+                                    </div>
+                                    <div className="col-4">
+                                        <Link to="#" onClick={() => logout()} className="font-poppins font-16 lh-12 regular pointer text-danger">
+                                            <div className="text-right mt-2">Cerrar</div>
+                                        </Link>
+                                    </div>
+                                </>
+                                : 
+
+                                <div className="col-md-12">
+                                    {
+                                        props.title ? 
+                                            <h1 className="base-panel-one-title">{props.title}</h1> 
+                                        : null
+                                    }
+                                </div>
+                            }
                             <div className="col-md-12">
                                 {
                                     props.children
