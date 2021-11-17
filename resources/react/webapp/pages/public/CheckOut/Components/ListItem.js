ad26e5a3b316a1e30cc6b4f5bbf47ed89a505cc5
Willywes <willywes@hotmail.cl>
2021-11-17 17:33:04 -0300

    [UPDATE] Footer

--- resources/react/webapp/pages/public/CheckOut/Components/ListItem.js
+++ resources/react/webapp/pages/public/CheckOut/Components/ListItem.js
@@ -8,7 +8,7 @@ const ListItem = ({address, showEdit, saveDefaultAddress, regions, communes}) =>
 
     return (
         <div className="row">
-            <div className="col-auto d-flex pr-0">
+            <div className="col-auto d-flex pr-0 pb-3">
                 <div className="my-auto">
                     <Form.Check
                         custom
@@ -23,7 +23,7 @@ const ListItem = ({address, showEdit, saveDefaultAddress, regions, communes}) =>
                     />
                 </div>
             </div>
-            <div className="col pl-0">
+            <div className="col pl-0 pb-3">
                 <div className="row">
                     <div className="col-12">
                         <span className="font-poppins font-10 regular color-8E8E8E">
@@ -31,14 +31,14 @@ const ListItem = ({address, showEdit, saveDefaultAddress, regions, communes}) =>
                         </span>
                     </div>
                     <div className="col-12">
-                        <span className="font-poppins font-16 regular color-484848">
-                            {address.address} , {address.extra_info ? address.extra_info+ ' , ' : ''} {commune ? commune.name : null} , {region ? region.name : null}
-                        </span>
+                        <div className="font-poppins font-14 lh-18 regular color-484848">
+                            {address.address} , {address.extra_info ? address.extra_info+ ', ' : ''} {commune ? commune.name : null}, {region ? region.name : null}
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="col-auto d-flex">
-                <div className="my-auto">
+                <div className="mt-auto">
                     <span onClick={() => showEdit(address)} className="link pointer font-12 regular">editar</span>
                 </div>
             </div>
