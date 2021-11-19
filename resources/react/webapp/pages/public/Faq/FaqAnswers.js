19917568399f26cecc84e0c4a5cf01b053ffc3a3
Willywes <willywes@hotmail.cl>
2021-11-19 14:59:15 -0300

    [UPDATE] Faqs

--- resources/react/webapp/pages/public/Faq/FaqAnswers.js
+++ resources/react/webapp/pages/public/Faq/FaqAnswers.js
@@ -12,29 +12,29 @@ const FaqAnswers = ({faqs}) => {
         fontSize: width >= 768 ? 16 : 14,
         fontFamily: 'Poppins, sans - serif !important',
         textDecoration: 'underline',
-        color: '#224F6B',
+        color: '#033F5D',
     }
 
     return(
             faqs.map((item, index) => {
                 let questionKey = uuidv4();
                 return (
-                    <Card key={questionKey} className="my-4" style={{borderRadius: 15}}> 
-                        <Card.Body> 
-                            <Collapsible 
-                                key={questionKey} 
+                    <Card key={questionKey} className="my-2" style={{borderRadius: 15}}>
+                        <Card.Body>
+                            <Collapsible
+                                key={questionKey}
                                 trigger={`${item.question}`}
                                 transitionTime={300}
                                 triggerStyle={triggerStyle}
                             >
-                                <div className="mt-3 font-poppins" style={{ fontSize: width >= 768 ? 14 : 12, color: '#949494'}} dangerouslySetInnerHTML={{ __html: item.answer }} />
+                                <div className="mt-3 font-poppins" style={{ fontSize: width >= 768 ? 14 : 12, color: '#6C6B6B'}} dangerouslySetInnerHTML={{ __html: item.answer }} />
                             </Collapsible>
-                        </Card.Body> 
-                    </Card> 
+                        </Card.Body>
+                    </Card>
                 )
             })
-        
-        
+
+
     );
 }
 
