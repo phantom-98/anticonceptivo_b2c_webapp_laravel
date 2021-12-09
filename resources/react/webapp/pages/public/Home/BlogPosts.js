db0edc638bc763c8c85fcf832fbea9ba4c92bc2b
Felipe <fe.ferngarc@gmail.com>
2021-12-09 15:32:35 -0300

    [ADD] carrousel blog post

--- resources/react/webapp/pages/public/Home/BlogPosts.js
+++ resources/react/webapp/pages/public/Home/BlogPosts.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { v4 as uuidv4, v4 as uuid } from "uuid";
+import React, {useContext} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import PostMiniCard from "../Blog/components/PostMiniCard";
+import { AppContext } from "../../../context/AppProvider";
+import { BREAKPOINTS } from "../../../helpers/vars";
 
 const BlogPosts = ({blogPosts, isLoaded}) => {
 
+    const { breakpoint } = useContext(AppContext)
+
     if (!blogPosts.length) return null;
+
+    const settings = {
+        className: "slider variable-width",
+        dots: true,
+        infinite: true,
+        speed: 3500,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false
+    };
 
     return (
         <div className="container mt-4 pt-2">
@@ -21,8 +39,22 @@ const BlogPosts = ({blogPosts, isLoaded}) => {
                                 </div>
 
                                 {
-                                    blogPosts.map(post => <div className="col-md-4 mb-3"><PostMiniCard post={post}
-                                        key={uuid()} /></div>)
+                                    breakpoint === BREAKPOINTS.MEDIUM || breakpoint === BREAKPOINTS.LARGE || breakpoint === BREAKPOINTS.EXTRA_LARGE || breakpoint === BREAKPOINTS.EXTRA_EXTRA_LARGE ?
+
+                                    blogPosts.map(post => <div key={uuidv4()} className="col-md-4 mb-3">
+                                        <PostMiniCard post={post} />
+                                    </div>)
+
+                                    : 
+                                        <div className="col-12 mb-4">
+                                            <Slider {...settings}>
+                                                {
+                                                    blogPosts.map(post => <div key={uuidv4()} className="col-md-4 mb-3">
+                                                        <PostMiniCard post={post} />
+                                                    </div>)
+                                                }
+                                            </Slider>
+                                        </div>
                                 }
                             </div>
                     }
