7fea0c07029d1cd13af46580786e79b51e7364c0
Felipe <fe.ferngarc@gmail.com>
2021-12-10 13:28:14 -0300

    [ADD] message bird

--- resources/views/webapp/base_react.blade.php
+++ resources/views/webapp/base_react.blade.php
@@ -76,10 +76,6 @@
 {{--        /></noscript>--}}
 {{--    <!-- End Facebook Pixel Code -->--}}
 
-    <script>
-        window.MessageBirdChatWidget.init()
-    </script>
-
     @yield('styles')
 
     <link rel="shortcut icon" href="/themes/web/favicon/favicon.ico" type="image/x-icon">
@@ -161,7 +157,6 @@
         <div id="app" style="overflow:hidden;">
             {!! $seo_preloading ?? '' !!}
         </div>
-
     @endif
 
         
@@ -169,15 +164,6 @@
 
 <script src="/themes/web/app/js/app.js"></script>
 <script src="/themes/web/assets/js/custom.js"></script>
-
-<script>
-    var MessageBirdChatWidgetSettings = { 
-        widgetId: '99b4c2e7-eceb-468b-8a80-a45733f14a17', 
-        initializeOnLoad: true, 
-    };
-
-    !function(){"use strict";if(Boolean(document.getElementById("live-chat-widget-script")))console.error("MessageBirdChatWidget: Snippet loaded twice on page");else{var e,t;window.MessageBirdChatWidget={},window.MessageBirdChatWidget.queue=[];for(var i=["init","setConfig","toggleChat","identify","hide","on","shutdown"],n=function(){var e=i[d];window.MessageBirdChatWidget[e]=function(){for(var t=arguments.length,i=new Array(t),n=0;n<t;n++)i[n]=arguments[n];window.MessageBirdChatWidget.queue.push([[e,i]])}},d=0;d<i.length;d++)n();var a=(null===(e=window)||void 0===e||null===(t=e.MessageBirdChatWidgetSettings)||void 0===t?void 0:t.widgetId)||"",o=function(){var e,t=document.createElement("script");t.type="text/javascript",t.src="https://livechat.messagebird.com/bootstrap.js?widgetId=".concat(a),t.async=!0,t.id="live-chat-widget-script";var i=document.getElementsByTagName("script")[0];null==i||null===(e=i.parentNode)||void 0===e||e.insertBefore(t,i)};"complete"===document.readyState?o():window.attachEvent?window.attachEvent("onload",o):window.addEventListener("load",o,!1)}}();
-</script>
 
 @yield('scripts')
 
