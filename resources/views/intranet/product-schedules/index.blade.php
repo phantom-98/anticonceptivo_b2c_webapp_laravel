519d590170ce52cbdf811fee962b978315fb25a6
Willywes <willywes@hotmail.cl>
2021-12-23 22:39:55 -0300

    [ADDED] Full Calendar Base

--- resources/views/intranet/product-schedules/index.blade.php
+++ resources/views/intranet/product-schedules/index.blade.php
@@ -42,6 +42,12 @@
     <link href="/themes/intranet/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet">
     <link href="/themes/intranet/plugins/fullcalendar/nifty-skin/fullcalendar-nifty.min.css" rel="stylesheet">
 
+    <style>
+        .fc-time-grid-container{
+            max-height: auto;
+        }
+    </style>
+
 @endsection
 
 @section('scripts')
@@ -49,110 +55,42 @@
     <script src="/themes/intranet/plugins/fullcalendar/lib/moment.min.js"></script>
     <script src="/themes/intranet/plugins/fullcalendar/lib/jquery-ui.custom.min.js"></script>
     <script src="/themes/intranet/plugins/fullcalendar/fullcalendar.min.js"></script>
+    <script src="/themes/intranet/plugins/fullcalendar/lang/es.js"></script>
+
 
     <script>
-        $(document).ready(() =>{
+        let schedules = [
+            {
+                title: '',
+                start: '2021-12-24 00:00:00',
+                end: '2021-12-24 12:00:00',
+                className: 'purple' // immediate
+            },
+            {
+                title: '',
+                start: '2021-12-24 12:00:00',
+                end: '2021-12-25 00:00:00',
+                className: 'red' // normal
+            },
+        ];
+        let schedulesNormal = [];
+    </script>
+
+    <script>
+        $(document).ready(() => {
             $('#calendar-immediate').fullCalendar({
+                defaultView: 'agendaWeek',
+                scrollTime: '08:00:00',
+                allDaySlot: false,
                 header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
+                    left: '',
+                    center: '',
+                    right: ''
                 },
-                editable: true,
-                droppable: true, // this allows things to be dropped onto the calendar
-                drop: function() {
-                    // is the "remove after drop" checkbox checked?
-                    if ($('#drop-remove').is(':checked')) {
-                        // if so, remove the element from the "Draggable Events" list
-                        $(this).remove();
-                    }
-                },
-                defaultDate: '2017-12-12',
-                eventLimit: true, // allow "more" link when too many events
-                events: [
-                    {
-                        title: 'Happy Hour',
-                        start: '2017-12-05',
-                        end: '2017-12-07',
-                        className: 'purple'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2017-12-15',
-                        end: '2017-12-17',
-                        className: 'mint'
-                    },
-                    {
-                        title: 'All Day Event',
-                        start: '2017-12-15',
-                        className: 'warning'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2017-12-20T10:30:00',
-                        end: '2017-12-20T12:30:00',
-                        className: 'danger'
-                    },
-                    {
-                        title: 'All Day Event',
-                        start: '2018-01-01',
-                        className: 'warning'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2018-01-07',
-                        end: '2018-01-10',
-                        className: 'purple'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2018-01-09T16:00:00'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2018-01-16T16:00:00',
-                        className: 'success'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2018-01-11',
-                        end: '2018-01-13',
-                        className: 'dark'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2018-01-12T10:30:00',
-                        end: '2018-01-12T12:30:00'
-                    },
-                    {
-                        title: 'Lunch',
-                        start: '2018-01-12T12:00:00',
-                        className: 'pink'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2018-01-12T14:30:00'
-                    },
-                    {
-                        title: 'Happy Hour',
-                        start: '2018-01-12T17:30:00'
-                    },
-                    {
-                        title: 'Dinner',
-                        start: '2018-01-12T20:00:00'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2018-01-13T07:00:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2018-01-28'
-                    }
-                ]
+                defaultDate: moment().format('YYYY-MM-DD'),
+                eventLimit: false, // allow "more" link when too many events
+                firstDay: 1,
+                events: schedules
             });
 
         })
