c6d76b19cf34c6f173a3a1f800769a11afb3dd53
xvad <xvad_@live.com>
2021-12-25 23:04:56 -0300

    ADDED: moment day of week

--- app/Http/Controllers/Intranet/ProductScheduleController.php
+++ app/Http/Controllers/Intranet/ProductScheduleController.php
@@ -41,27 +41,37 @@ class ProductScheduleController extends GlobalController
             ProductSchedule::truncate();
 
             foreach ($events as $event) {
-                $productSchedule = new ProductSchedule();
                 $start_time = Carbon::parse($event['start']);
                 $end_time = Carbon::parse($event['end']);
                 $period = CarbonPeriod::create($start_time->format('Y-m-d'), $end_time->format('Y-m-d'));
 
+
                 foreach ($period as $key => $date) {
+                    $productSchedule = new ProductSchedule();
+
                     if($key == 0){
                         $productSchedule->start_time = $start_time->format('H:i:s');
                         $productSchedule->end_time = '23:59:59';
                         $productSchedule->day_of_week = $date->dayOfWeek;
-                    }elseif ($key == count($period) - 1){
-                        $productSchedule->start_time = '00:00:00';
-                        $productSchedule->end_time = $end_time->format('H:i:s');
-                        $productSchedule->day_of_week = $date->dayOfWeek;
-                    }else{
-                        $productSchedule->start_time = '00:00:00';
-                        $productSchedule->end_time = '23:59:59';
-                        $productSchedule->day_of_week = $date->dayOfWeek;
+//                    }elseif ($key == count($period) - 1){
+//                        $productSchedule->start_time = '00:00:00';
+//                        $productSchedule->end_time = $end_time->format('H:i:s');
+//                        $productSchedule->day_of_week = $date->dayOfWeek;
                     }
+//                    }else{
+//                        $productSchedule->start_time = '00:00:00';
+//                        $productSchedule->end_time = '23:59:59';
+//                        $productSchedule->day_of_week = $date->dayOfWeek;
+//                    }
                     $productSchedule->type = $event['type'];
                     $productSchedule->save();
+
+                    if($start_time->format('Y-m-d') != $end_time->format('Y-m-d') && $end_time->format('H:i:s') != '00:00:00'){
+                        $productSchedule->start_time = '00:00:00';
+                        $productSchedule->end_time = $end_time->format('H:i:s');
+                        $productSchedule->day_of_week = $date->dayOfWeek + 1;
+                    }
+
                 }
             }
 
