31079301aff3b50fa513fa1c3b830f702824cf7f
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2021-12-29 10:05:59 -0300

    Update Addresses.js

--- resources/react/webapp/pages/public/CheckOut/Addresses.js
+++ resources/react/webapp/pages/public/CheckOut/Addresses.js
@@ -138,8 +138,8 @@ const Addresses = ({setView, regions, communes, address, setAddress, dispatchDat
                                        Hora de entrega
                                     </span>
                                 </div>
-                                <div className="col-auto d-flex">
-                                    <span className="my-auto font-poppins font-14 regular color-484848">
+                                <div className="col-auto mx-2 d-flex">
+                                    <span className="my-auto font-poppins font-14 regular color-484848" style={{marginLeft: '10px'}}>
                                          {dispatchDateObject ?  (dispatchDateObject?.is_immediate ? 'Hasta 2 horas después de tu compra. Despachos desde 10:00 a 19:00 hrs.' : dispatchDateObject?.schedule.formated_start_time + ' a ' + dispatchDateObject?.schedule.formated_end_time) : ''}
                                     </span>
                                 </div>
