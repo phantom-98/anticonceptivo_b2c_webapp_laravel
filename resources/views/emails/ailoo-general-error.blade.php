6e80b5a7f3df7a9734821dc067a313436bcc7643
xvad <xvad_@live.com>
2022-03-25 18:04:57 -0300

    FIX: cambio en correo de error ailo y a se agrega log

--- resources/views/emails/ailoo-general-error.blade.php
+++ resources/views/emails/ailoo-general-error.blade.php
@@ -5,7 +5,7 @@
     <div class="titulo">Hola {{ $user_name }}:</div>
     <div class="mensaje">
         <p class="text-legal">
-            Error inesperado conexión con ailoo en proceso de pago
+            Error inesperado conexión con ailoo al consultar stock en proceso de pago para el usuario {{ $labelUser }}
         </p>
 
 
