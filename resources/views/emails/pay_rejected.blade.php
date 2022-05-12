c906f3a2c5388606d192b020e438ba64981016b0
xvad <xvad_@live.com>
2022-05-12 13:37:48 -0400

    FIX

--- resources/views/emails/pay_rejected.blade.php
+++ resources/views/emails/pay_rejected.blade.php
@@ -1,11 +1,11 @@
 @extends('emails.base')
-@section('header', 'Recuperar Contraseña')
+@section('header', 'Pago Suscripción')
 @section('content')
 
     <div class="title">Estimado/a {{ $full_name }} </div>
     <div class="message">
         <p>
-            Se ha rechazado el pago de su tarjeta favor actualizar su método de pago en su cuenta,
+            Se ha rechazado el pago de su tarjeta para el pago de su suscripción favor actualizar su método de pago en su cuenta,
             haga click <a href="https://anticonceptivo.cl/mi-cuenta/suscripcion" target="_blank" class="text-secondary"> aquí</a> para cambiar tarjeta asociada agregando su tarjeta de débito o crédito
         </p>
     </div>
