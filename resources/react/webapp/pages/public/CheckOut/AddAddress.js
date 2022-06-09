76a6afbc21aa53c69d90450acadec13c3d89469c
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-06-09 13:27:48 -0400

    [ADD] Campo adicional intranet

--- resources/react/webapp/pages/public/CheckOut/AddAddress.js
+++ resources/react/webapp/pages/public/CheckOut/AddAddress.js
@@ -230,7 +230,7 @@ const AddAddress = ({setView, regions, address, setAddress,validAddress,setValid
                                        className="form-control form-control-custom"
                                        id="comment"
                                        name="comment"
-                                       placeholder="Comentario"
+                                       placeholder="Agrega un rango de horario para la entrega, es en oficina o una casa, u cualquier otra información relevante como el detalle de como llegar"
                                        value={address.comment}
                                        onChange={(e) => handleAddressComment(e)}
                                        onFocus={setCleanInputError}
