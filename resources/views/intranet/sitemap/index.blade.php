1bdd811eee251a5a2ac38033b5543303135bfd50
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-07-27 18:07:54 -0400

    Update index.blade.php

--- resources/views/intranet/sitemap/index.blade.php
+++ resources/views/intranet/sitemap/index.blade.php
@@ -2,46 +2,46 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
-        <loc>https://www.anticonceptivo.cl</loc>
+        <loc>{{ env('APP_URL') }}</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/sobre-nosotros</loc>
+        <loc>{{ env('APP_URL') }}/sobre-nosotros</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/contactanos</loc>
+        <loc>{{ env('APP_URL') }}/contactanos</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/preguntas-frecuentes</loc>
+        <loc>{{ env('APP_URL') }}/preguntas-frecuentes</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/responsabilidad-empresarial/politicas-de-privacidad</loc>
+        <loc>{{ env('APP_URL') }}/responsabilidad-empresarial/politicas-de-privacidad</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/responsabilidad-empresarial/carta-de-desabastecimiento</loc>
+        <loc>{{ env('APP_URL') }}/responsabilidad-empresarial/carta-de-desabastecimiento</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/responsabilidad-empresarial/plazos-y-costos-entrega</loc>
+        <loc>{{ env('APP_URL') }}/responsabilidad-empresarial/plazos-y-costos-entrega</loc>
         <changefreq>daily</changefreq>
     </url>
     <url>
-        <loc>https://www.anticonceptivo.cl/terminos-y-condiciones</loc>
+        <loc>{{ env('APP_URL') }}/terminos-y-condiciones</loc>
         <changefreq>daily</changefreq>
     </url>
     @foreach($categories as $category)
         <url>
-            <loc>https://www.anticonceptivo.cl/tienda/{{ $category->slug }}</loc>
+            <loc>{{ env('APP_URL') }}/tienda/{{ $category->slug }}</loc>
             <changefreq>daily</changefreq>
         </url>
         @if($category->slug != "pastillas")
             @foreach($category->subcategories as $sub)
                 <url>
-                    <loc>https://www.anticonceptivo.cl/tienda/{{ $category->slug }}/{{ $sub->slug }}</loc>
+                    <loc>{{ env('APP_URL') }}/tienda/{{ $category->slug }}/{{ $sub->slug }}</loc>
                     <changefreq>daily</changefreq>
                 </url>
             @endforeach
@@ -49,19 +49,19 @@
         @if($category->slug == "pastillas")
             @foreach($formats as $format)
                 <url>
-                    <loc>https://www.anticonceptivo.cl/tienda/{{ $category->slug }}/formato/{{ $format }}</loc>
+                    <loc>{{ env('APP_URL') }}/tienda/{{ $category->slug }}/formato/{{ $format }}</loc>
                     <changefreq>daily</changefreq>
                 </url>
             @endforeach
             @foreach($subscriptions as $subscription)
                 <url>
-                    <loc>https://www.anticonceptivo.cl/tienda/{{ $category->slug }}/suscripcion/{{ $subscription->months }}</loc>
+                    <loc>{{ env('APP_URL') }}/tienda/{{ $category->slug }}/suscripcion/{{ $subscription->months }}</loc>
                     <changefreq>daily</changefreq>
                 </url>
             @endforeach
             @foreach($laboratories as $laboratory)
                 <url>
-                    <loc>https://www.anticonceptivo.cl/tienda/{{ $category->slug }}/laboratorio/{{ \Str::slug($laboratory->name) }}</loc>
+                    <loc>{{ env('APP_URL') }}/tienda/{{ $category->slug }}/laboratorio/{{ \Str::slug($laboratory->name) }}</loc>
                     <changefreq>daily</changefreq>
                 </url>
             @endforeach
@@ -69,18 +69,18 @@
     @endforeach
     @foreach($products as $product)
         <url>
-            <loc>https://www.anticonceptivo.cl/producto/{{ $product->slug }}</loc>
+            <loc>{{ env('APP_URL') }}/producto/{{ $product->slug }}</loc>
             <changefreq>daily</changefreq>
         </url>
     @endforeach
     @foreach($post_types as $type)
         <url>
-            <loc>https://www.anticonceptivo.cl/blog/{{ $type->slug }}</loc>
+            <loc>{{ env('APP_URL') }}/blog/{{ $type->slug }}</loc>
             <changefreq>daily</changefreq>
         </url>
         @foreach($type->active_posts as $post)
             <url>
-                <loc>https://www.anticonceptivo.cl/blog/{{ $type->slug }}/post/{{$post->slug}}</loc>
+                <loc>{{ env('APP_URL') }}/blog/{{ $type->slug }}/post/{{$post->slug}}</loc>
                 <changefreq>daily</changefreq>
             </url>
         @endforeach
