cb5da9aa928eb50748653f70cd93392127722a6e
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-09-12 19:14:50 -0300

    a

--- public/themes/intranet/js_bu/includes/bootstrapTable.js
+++ public/themes/intranet/js_bu/includes/bootstrapTable.js
@@ -37,8 +37,25 @@ function midAling(value, row, index, field) {
     };
 }
 
-$('#table-bs').on('all.bs.table', function (d)
+$('#table-bs').on('page-change.bs.table', function (d)
 {
+    runActiveControl();
+});
+
+$('#table-bs').on('column-switch.bs.table', function (d) {
+
+    runActiveControl();
+});
+
+$('#table-bs').on('all.bs.table', function (d) {
+    runActiveControl();
+});
+
+$('#table-bs').on('search.bs.table', function (d) {
+    runActiveControl();
+});
+
+$('#table-bs').on('sort.bs.table', function (d) {
     runActiveControl();
 });
 
