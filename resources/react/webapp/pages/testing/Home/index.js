22a23db64cb618c6b37dcc765bcc758b36b8d223
Felipe Fernández <fe.ferngarc@gmail.com>
2022-09-27 15:49:21 -0300

    [ADD] base for refactoring

---
+++ resources/react/webapp/pages/testing/Home/index.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import * as Services from "../../../Services";
+import BannerCarousel from "../../../components/sections/BannerCarousel";
+import LazyLoading from '../../../components/LazyLoading';
+
+const Home = ({match}) => {
+    const [topBanners, setTopBanners] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const getData = () => {
+        let url = Services.ENDPOINT.PUBLIC_AREA.BANNERS.HOME.TOP;
+        let data = {}
+        Services.DoGet(url, data).then(response => {
+            Services.Response({
+                response: response,
+                success: () => {
+                    setTopBanners(response.data.top_banners);
+                    setIsLoaded(true);
+                },
+            });
+        }).catch(error => {
+            Services.ErrorCatch(error)
+        });
+    }
+
+    if (!isLoaded) {
+        return (
+            <LazyLoading />
+        )
+    }
+
+    return (
+        <>
+            <div className="bg-FAFAFA">
+                <BannerCarousel topBanners={topBanners}/>
+            </div>
+            <div className="col-12 py-4">
+
+            </div>
+        </>
+    );
+};
+
+export default Home;
