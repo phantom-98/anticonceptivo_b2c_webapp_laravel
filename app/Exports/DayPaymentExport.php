ef14de89c19fdb819625aad01ed8d2d096b688ca
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-09-29 18:10:44 -0300

    Update DayPaymentExport.php

--- app/Exports/DayPaymentExport.php
+++ app/Exports/DayPaymentExport.php
@@ -21,27 +21,7 @@ class DayPaymentExport implements FromView, ShouldAutoSize
     */
     public function view(): View
     {
-        $end = Carbon::parse($this->startFilter)->endOfMonth()->format('Y-m-d');
-        $start = Carbon::parse($this->endFilter)->startOfMonth()->format('Y-m-d');
-        if ($date) {
-            if (strpos($date, "-")) {
-                $start = substr($date, 0, strpos($date, "-"));
-                $start = str_replace(" ", "", $start);
-                $start = str_replace("/", "-", $start);
-                $start = Carbon::parse($start)->format('Y-m-d');
-                $end = substr($date, strpos($date, "-"), strlen($date));
-                $end = str_replace("- ", "", $end);
-                $end = str_replace("/", "-", $end);
-                $end = Carbon::parse($end)->format('Y-m-d');
-            } else {
-                $start = str_replace(" ", "", $date);
-                $start = str_replace("/", "-", $start);
-                $start = Carbon::parse($start)->format('Y-m-d');
-                $end = Carbon::parse($start)->format('Y-m-d');
-            }
-        }
-
-        $objects = DayPayment::whereBetween('date_payment', [$start.' 00:00:00', $end.' 23:59:59'])->orderBy('date_payment')->get();
+        $objects = DayPayment::whereBetween('date_payment', [$this->startFilter.' 00:00:00', $this->endFilter.' 23:59:59'])->orderBy('date_payment')->get();
         return view('intranet.exports.day_payments')->with('objects', $objects);
     }
 
