e667b71c6e1eb138f5f5b39ce012459a950ead87
Felipe Fernández <fe.ferngarc@gmail.com>
2022-10-24 09:15:54 -0300

    [ADD] discount

--- resources/react/webapp/components/shopping/TotalCartPrice.js
+++ resources/react/webapp/components/shopping/TotalCartPrice.js
@@ -6,19 +6,23 @@ const TotalCartPrice = () =>{
 
     const {cartItems} = useContext(CartContext);
     const [totalCart, setTotalCart] = useState(0);
+    const [totalDiscount, setTotalDiscount] = useState(0);
 
     useEffect(()=>{
         let _total = 0;
+        let _total_discount = 0;
         cartItems.map((item) =>{
+            console.log('item', item);
             if(item.subscription != null){
                 _total = _total + (item.quantity * item.subscription.price * item.subscription.quantity)
-                
+                _total_discount = _total_discount + (item.quantity * (item.product.price - item.subscription.price))
             }else{
                 _total = _total + (item.quantity * (item.product.is_offer ? item.product.offer_price : item.product.price))
-
+                _total_discount = _total_discount + (item.quantity * (item.product.price - (item.product.is_offer ? item.product.offer_price : item.product.price)))
             }
         })
         setTotalCart(_total);
+        setTotalDiscount(_total_discount);
     },[cartItems])
 
     return (
@@ -29,6 +33,14 @@ const TotalCartPrice = () =>{
                 </div>
                 <div className="col text-right">
                     <span className="font-poppins font-16 regular color-1F1F1F text-uppercase">{formatMoney(totalCart)}</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-auto">
+                    <span className="font-poppins font-16 regular color-1F1F1F text-uppercase">Descuento</span>
+                </div>
+                <div className="col text-right">
+                    <span className="font-poppins font-16 regular color-1F1F1F text-uppercase">{formatMoney(totalDiscount)}</span>
                 </div>
             </div>
             <div className="row">
