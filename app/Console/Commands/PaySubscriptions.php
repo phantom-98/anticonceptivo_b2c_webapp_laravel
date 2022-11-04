707b1e64308ab81f73765cc45148a52fe58b4783
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-11-04 17:58:13 -0300

    Update PaySubscriptions.php

--- app/Console/Commands/PaySubscriptions.php
+++ app/Console/Commands/PaySubscriptions.php
@@ -590,20 +590,6 @@ class PaySubscriptions extends Command
 
             $stringProduct = rtrim($stringProduct, ", ");
 
-            $sendgrid = new \SendGrid(env('SENDGRID_APP_KEY'));
-
-            // Envio al cliente
-            $html = view('emails.pay_rejected', ['full_name' => $customer->first_name . " " . $customer->last_name, 'id_number' => $customer->id_number, 'stringProduct' => $stringProduct])->render();
-
-            $email = new \SendGrid\Mail\Mail();
-            $email->setFrom("info@anticonceptivo.cl", 'Anticonceptivo');
-            $email->setSubject('No Pago suscripción');
-            $email->addTo($customer->email, $customer->first_name . " " . $customer->last_name);
-            $email->addContent(
-                "text/html", $html
-            );
-
-            $sendgrid->send($email);
 
             $sendgrid = new \SendGrid(env('SENDGRID_APP_KEY'));
             $email = new \SendGrid\Mail\Mail();
@@ -616,20 +602,6 @@ class PaySubscriptions extends Command
 
             $sendgrid->send($email);
 
-
-            $users = User::where('id','!=' ,1)->get();
-            foreach($users as $user){
-                $sendgrid = new \SendGrid(env('SENDGRID_APP_KEY'));
-                $html = view('emails.pay_rejected', ['full_name' => $customer->first_name . " " . $customer->last_name, 'id_number' => $customer->id_number, 'stringProduct' => $stringProduct])->render();
-                $email = new \SendGrid\Mail\Mail();
-                $email->setFrom("info@anticonceptivo.cl", 'Anticonceptivo');
-                $email->setSubject('No Pago suscripción');
-                $email->addTo($user->email, $user->first_name);
-                $email->addContent(
-                    "text/html", $html
-                );
-                $sendgrid->send($email);
-            }
         }
 
     }
