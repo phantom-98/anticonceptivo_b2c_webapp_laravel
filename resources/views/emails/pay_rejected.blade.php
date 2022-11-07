f5ef0c4df9d38cd9dd59ac806e36fb3a34c3e61c
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-11-07 00:55:53 -0300

    Update pay_rejected.blade.php

--- resources/views/emails/pay_rejected.blade.php
+++ resources/views/emails/pay_rejected.blade.php
@@ -2,7 +2,7 @@
 @section('header', 'No Pago Suscripción')
 @section('content')
 
-    <div class="title">Hola {{ $full_name }} <span style="font-size: 14px;">({{ $id_number }})</span> </div>
+    <div class="title">Hola {{ ucwords(mb_strtolower($full_name, 'UTF-8')) }} <span style="font-size: 14px;">({{ $id_number }})</span> </div>
     <div class="message">
         <p>
             Lamentamos informarte que se ha rechazado el pago de tu suscripción ({{$stringProduct}}). Recuerda puedes ingresar a tu cuenta en <a href="https://anticonceptivo.cl/" target="_BLANK">anticonceptivo.cl</a> para actualizar los datos de tu tarjeta (aceptamos tarjetas de Débito y Crédito) y seguir eligiendo los mejores Beneficios.
