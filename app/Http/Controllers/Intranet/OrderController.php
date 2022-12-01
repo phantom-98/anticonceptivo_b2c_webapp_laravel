57c6ef5ce5b8714cd729cd28311ff7a7906e8eaa
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-12-01 17:14:22 -0300

    [ADD] Laboratorios

--- app/Http/Controllers/Intranet/OrderController.php
+++ app/Http/Controllers/Intranet/OrderController.php
@@ -37,7 +37,7 @@ class OrderController extends GlobalController
 
     public function index(Request $request)
     {
-        $objects = Order::with(['customer', 'prescriptions.product','subscriptions_orders_items.commune', 'order_items.product']);
+        $objects = Order::with(['customer', 'prescriptions.product','subscriptions_orders_items.commune', 'order_items.product.laboratory']);
         $clients = Customer::get();
 
         $date = $request->date;
