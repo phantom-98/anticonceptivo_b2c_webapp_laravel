ef899e712b0acfe9f808af3194466a1e65d5f016
Felipe Peñailillo <fpenailillo@innovaweb.cl>
2022-12-27 19:28:24 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('command:paySubscriptions')->dailyAt('00:30');
         //$schedule->command('command:updateStateDispatch')->everyFiveMinutes();
         $schedule->command('command:sitemap')->dailyAt('01:00');
-        $schedule->command('command:expiration-subscription-email')->dailyAt('10:00');
+        $schedule->command('command:expiration-subscription-email')->dailyAt('08:00');
     }
 
     /**
