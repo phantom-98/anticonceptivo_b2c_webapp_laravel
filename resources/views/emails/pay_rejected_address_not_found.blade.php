ef529e2b1958998078713874608f11ed26ab55b7
Felipe PC <fe.ferngarc@gmail.com>
2022-12-28 09:31:55 -0300

    [ADD] email rejected by address ready

--- resources/views/emails/pay_rejected_address_not_found.blade.php
+++ resources/views/emails/pay_rejected_address_not_found.blade.php
@@ -5,13 +5,11 @@
     <div class="title">Hola {{ ucwords(mb_strtolower($full_name, 'UTF-8')) }} <span style="font-size: 14px;">({{ $id_number }})</span> </div>
     <div class="message">
         <p>
-            {{-- Lamentamos informarte que se ha rechazado el pago de tu suscripción ({{$stringProduct}}). Recuerda puedes actualizar tu método de pago en 3 simples pasos:
+            Lamentamos informarte que se ha rechazado el pago de tu suscripción ({{$stringProduct}}) ya que no se encuentra asociada una dirección a la suscripción. Recuerda puedes actualizar tu dirección en 2 simples pasos:
             <br/><br/>
             1) Ingresa a tu cuenta en <a href="https://anticonceptivo.cl/" target="_BLANK">anticonceptivo.cl</a>
             <br/>
-            2) En <b>Tarjetas de Pago</b> podrás actualizar tus tarjetas. Se aceptan todo tipo de tarjetas: Débito, Crédito o Prepago - Mastercard, Visa o Transbank.
-            <br/>
-            3) Validar en <b>Suscripciones</b> todos tus datos están correctos: Fecha Despacho, Dirección y <b>Tarjeta Asociada!</b>.
+            2) En <b>Suscripciones</b> podrás actualizar la dirección de tu suscripción.
             <br/><br/>
             Por último, seguir disfrutando de los mejores beneficios.
             <br/><br/>
@@ -21,7 +19,7 @@
             <br/><br/>
             Un gran saludo,
             <br/>
-            Equipo <a href="https://anticonceptivo.cl/" target="_BLANK">anticonceptivo.cl</a> --}}
+            Equipo <a href="https://anticonceptivo.cl/" target="_BLANK">anticonceptivo.cl</a>
         </p>
     </div>
 
