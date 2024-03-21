2c81ae3b9d9b90020b78cc31ef04b1603f1c8296
samuelborraccini <samuelborraccini@hotmail.com>
2024-03-21 13:36:14 -0300

    fix route

--- routes/api/v1/application/payment.php
+++ routes/api/v1/application/payment.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\App\Payment\PaymentController;
-use App\Http\Controllers\Api\v1\App\Payment\WebCheckoutController;
+use App\Http\Controllers\Api\V1\App\Payment\WebCheckoutController;
 use App\Http\Controllers\Api\V1\App\Payment\WebpayPlusController;
 use Illuminate\Support\Facades\Route;
 
